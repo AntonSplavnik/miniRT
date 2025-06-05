f35ef33d99883d761fbf3e02287294c8f3e356f3
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-06-05 11:12:50 +0200

    Bumpmap parsing more efficient

--- menger_sponge/cleaned_version/srcs/parser/parser_bumpmap.c
+++ menger_sponge/cleaned_version/srcs/parser/parser_bumpmap.c
@@ -6,77 +6,74 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 14:06:23 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/04 17:10:32 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/05 11:02:22 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-// Helper function to read all remaining data from file and tokenize it
-char **read_and_tokenize_ppm_data(int fd)
+// Helper function to skip whitespace and find next number
+static char *skip_whitespace(char *str)
 {
-	char *buffer;
-	char *temp_line;
-	char **tokens;
-	size_t buffer_size = 0;
-	size_t buffer_capacity = 4096;
-
-	// Allocate initial buffer
-	buffer = malloc(buffer_capacity);
-	if (!buffer)
-		return (NULL);
-	buffer[0] = '\0';
-
-	// Read all remaining lines and concatenate them
-	while ((temp_line = get_next_line(fd)) != NULL)
-	{
-		size_t line_len = ft_strlen(temp_line);
-
-		// Ensure buffer has enough space
-		while (buffer_size + line_len + 2 > buffer_capacity)
-		{
-			buffer_capacity *= 2;
-			char *new_buffer = realloc(buffer, buffer_capacity);
-			if (!new_buffer)
-			{
-				free(buffer);
-				free(temp_line);
-				return (NULL);
-			}
-			buffer = new_buffer;
-		}
-
-		// Append line to buffer with space separator
-		if (buffer_size > 0)
-		{
-			buffer[buffer_size] = ' ';
-			buffer_size++;
-		}
-		ft_strlcpy(buffer + buffer_size, temp_line, line_len + 1);
-		buffer_size += line_len;
-
-		free(temp_line);
-	}
-
-	// Tokenize the entire buffer by spaces/whitespace
-	tokens = ft_split(buffer, ' ');
-	free(buffer);
-
-	return (tokens);
+	while (*str && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r'))
+		str++;
+	return (str);
 }
 
-// Helper function to count tokens in array
-int count_tokens(char **tokens)
+// Helper function to parse integer from string and advance pointer
+static int parse_next_int(char **str)
 {
-	int count = 0;
+	int result = 0;
+	int sign = 1;
 
-	if (!tokens)
-		return (0);
+	*str = skip_whitespace(*str);
 
-	while (tokens[count])
-		count++;
+	if (**str == '-')
+	{
+		sign = -1;
+		(*str)++;
+	}
+	else if (**str == '+')
+		(*str)++;
 
-	return (count);
+	while (**str >= '0' && **str <= '9')
+	{
+		result = result * 10 + (**str - '0');
+		(*str)++;
+	}
+
+	return (result * sign);
+}
+
+// Fast bulk reading function
+static char *read_remaining_file(int fd, size_t *size)
+{
+	char buffer[8192];
+	char *result = NULL;
+	char *temp;
+	size_t total_size = 0;
+	ssize_t bytes_read;
+
+	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+		temp = realloc(result, total_size + bytes_read + 1);
+		if (!temp)
+		{
+			free(result);
+			return (NULL);
+		}
+		result = temp;
+		ft_memcpy(result + total_size, buffer, bytes_read);
+		total_size += bytes_read;
+	}
+
+	if (result)
+	{
+		result[total_size] = '\0';
+		*size = total_size;
+	}
+
+	return (result);
 }
 
 t_bump_map	*create_bump_map(t_scene *scene, const char *filename)
@@ -84,12 +81,12 @@ t_bump_map	*create_bump_map(t_scene *scene, const char *filename)
 	t_bump_map	*bump_map;
 	int			fd;
 	char		*line;
-	int			width;
-	int			height;
+	int			width, height;
 	int			inline_position;
-	char		**rgb_tokens;
-	int			token_count;
-	int			expected_values;
+	char		*data_buffer;
+	char		*current_pos;
+	size_t		buffer_size;
+	int			i, total_pixels;
 
 	bump_map = malloc(sizeof(t_bump_map));
 	if (!bump_map)
@@ -153,7 +150,8 @@ t_bump_map	*create_bump_map(t_scene *scene, const char *filename)
 	bump_map->height = height;
 
 	// Allocate height data
-	bump_map->height_data = malloc(width * height * sizeof(float));
+	total_pixels = width * height;
+	bump_map->height_data = malloc(total_pixels * sizeof(float));
 	if (!bump_map->height_data)
 	{
 		free(bump_map->filename);
@@ -168,62 +166,54 @@ t_bump_map	*create_bump_map(t_scene *scene, const char *filename)
 	{
 		if (line)
 			free(line);
-		free(bump_map->filename);
-		free(bump_map);
+		free_bump_map(bump_map);
 		close(fd);
 		parse_error(scene, "Invalid max color value in PPM file");
 	}
 	free(line);
 
-	// Read and tokenize all RGB data
-	expected_values = width * height * 3; // 3 values per pixel
-	rgb_tokens = read_and_tokenize_ppm_data(fd);
+	// Read remaining file data in bulk
+	data_buffer = read_remaining_file(fd, &buffer_size);
 	close(fd);
 
-	if (!rgb_tokens)
+	if (!data_buffer)
 	{
-		free(bump_map->filename);
-		free(bump_map);
+		free_bump_map(bump_map);
 		parse_error(scene, "Could not read RGB data from PPM file");
 	}
 
-	token_count = count_tokens(rgb_tokens);
-	if (token_count != expected_values)
+	// Process RGB values directly from buffer
+	current_pos = data_buffer;
+	i = 0;
+	while (i < total_pixels && current_pos < data_buffer + buffer_size)
 	{
-		free_split(rgb_tokens);
-		free(bump_map->filename);
-		free(bump_map);
-		parse_error(scene, "Invalid number of RGB values in PPM file");
-	}
-
-	// Process RGB values
-	int i = 0;
-	int token_idx = 0;
-	while (i < width * height && token_idx + 2 < token_count)
-	{
-		int r = ft_atoi(rgb_tokens[token_idx]);
-		int g = ft_atoi(rgb_tokens[token_idx + 1]);
-		int b = ft_atoi(rgb_tokens[token_idx + 2]);
+		int r = parse_next_int(&current_pos);
+		int g = parse_next_int(&current_pos);
+		int b = parse_next_int(&current_pos);
 
 		// Validate RGB values
 		if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
 		{
-			free_split(rgb_tokens);
-			free(bump_map->filename);
-			free(bump_map);
+			free(data_buffer);
+			free_bump_map(bump_map);
 			parse_error(scene, "Invalid RGB values in PPM file");
 		}
 
 		// Convert RGB to grayscale height value
-		float normalized_value = (r + g + b) / 3.0f / 255.0f; // Should be [0.0, 1.0]
-		bump_map->height_data[i] = (normalized_value - 0.5f) * 0.1f; // Convert to [-0.05, 0.05]
+		float normalized_value = (r + g + b) / (3.0f * 255.0f);
+		// Scale to [-0.05, 0.05] range because bump maps typically use small height variations
+		bump_map->height_data[i] = (normalized_value - 0.5f) * 0.1f;
 
 		i++;
-		token_idx += 3;
 	}
 
-	// Clean up tokens
-	free_split(rgb_tokens);
+	free(data_buffer);
+
+	if (i != total_pixels)
+	{
+		free_bump_map(bump_map);
+		parse_error(scene, "Invalid number of RGB values in PPM file");
+	}
 
 	return (bump_map);
 }
