a9443446369c0cc3b58e1b184939fcbd805eca04
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-06-06 14:38:49 +0200

    Parser_light.c norminette ok

--- menger_sponge/cleaned_version/srcs/parser/parser_light.c
+++ menger_sponge/cleaned_version/srcs/parser/parser_light.c
@@ -6,29 +6,24 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:30:00 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/03 10:13:06 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/06 14:38:10 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-int parse_light(t_scene *scene, char *line)
+t_light	*parse_light_properties(t_scene *scene, char **parts)
 {
-	char	**parts;
-	int		parse_result;
-	t_light *new_light;
-	t_vec3  position;
-	double  intensity;
-	t_color color;
+	t_vec3	position;
+	double	intensity;
+	t_color	color;
+	int		parse_color_result;
+	t_light	*new_light;
 
-	parts = ft_split_line(line, ' ');
-	if (!parts)
-		parse_error(scene, "Failed to split line");
-	check_parts_count(scene, parts, 4, "light");
 	if (!read_vector(parts[1], &position))
 	{
 		free_split(parts);
-		parse_error(scene, "Invalid format for light position. Expected: x,y,z");
+		parse_error(scene, "Expected for light position: x,y,z");
 	}
 	intensity = ft_atof(parts[2]);
 	if (!check_range(intensity, 0.0, 1.0))
@@ -36,14 +31,26 @@ int parse_light(t_scene *scene, char *line)
 		free_split(parts);
 		parse_error(scene, "Light ratio must be between 0.0 and 1.0");
 	}
-	parse_result = read_color(parts[3], &color);
+	parse_color_result = read_color(parts[3], &color);
 	free_split(parts);
-	if (!parse_result)
+	if (!parse_color_result)
 		parse_error(scene, "Invalid format for light color. Expected: r,g,b");
-
 	new_light = create_light(position, intensity, color);
 	if (!new_light)
 		parse_error(scene, "Failed to create light");
+	return (new_light);
+}
+
+int	parse_light(t_scene *scene, char *line)
+{
+	char	**parts;
+	t_light	*new_light;
+
+	parts = ft_split_line(line, ' ');
+	if (!parts)
+		parse_error(scene, "Failed to split line");
+	check_parts_count(scene, parts, 4, "light");
+	new_light = parse_light_properties(scene, parts);
 	add_light(scene, new_light);
 	return (1);
 }
