90540387eda9c956eac1abe9e39ef3137cb023b7
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-06-06 16:06:52 +0200

    parser triangle and parser cube ok

--- menger_sponge/cleaned_version/srcs/parser/parser_cube.c
+++ menger_sponge/cleaned_version/srcs/parser/parser_cube.c
@@ -6,24 +6,25 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:30:00 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/06 15:00:34 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/06 16:05:18 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-void	parse_cube_data(t_scene *scene, char **parts, t_vec3 *center, double *side_length, t_color *color)
+void	parse_cube_data(t_scene *scene, char **parts, \
+							t_cube *cube_data, t_color *color)
 {
-	if (!read_vector(parts[1], center))
+	if (!read_vector(parts[1], &cube_data->center))
 	{
 		free_split(parts);
 		parse_error(scene, "Invalid format for cube center. Expected: x,y,z");
 	}
-	*side_length = ft_atof(parts[2]);
-	if (!check_range(*side_length, 0.0, INFINITY))
+	cube_data->side_length = ft_atof(parts[2]);
+	if (!check_range(cube_data->side_length, 0.0, INFINITY))
 	{
 		free_split(parts);
-		parse_error(scene, "Invalid format for cube side length. Expected: positive number");
+		parse_error(scene, "Expected positive number for cube side length.");
 	}
 	if (!read_color(parts[3], color))
 	{
@@ -36,8 +37,7 @@ int	parse_cube(t_scene *scene, char *line)
 {
 	char		**parts;
 	t_object	*cube;
-	t_vec3		center;
-	double		side_length;
+	t_cube		cube_data;
 	t_color		color;
 	char		*material_block;
 
@@ -48,12 +48,12 @@ int	parse_cube(t_scene *scene, char *line)
 	if (!parts)
 		parse_error(scene, "Failed to split line");
 	check_parts_count(scene, parts, 4, "cube");
-	parse_cube_data(scene, parts, &center, &side_length, &color);
+	parse_cube_data(scene, parts, &cube_data, &color);
 	free_split(parts);
-	cube = create_cube(center, side_length, color);
+	cube = create_cube(cube_data.center, cube_data.side_length, color);
 	if (!cube)
 		parse_error(scene, "Failed to create cube");
-	if(material_block)
+	if (material_block)
 		parse_material_properties(scene, material_block, &cube->material);
 	add_object(scene, cube);
 	return (1);
--- menger_sponge/cleaned_version/srcs/parser/parser_triangle.c
+++ menger_sponge/cleaned_version/srcs/parser/parser_triangle.c
@@ -6,38 +6,42 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:30:00 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/06 15:01:06 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/06 15:45:58 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-void	parse_triangle_data(t_scene *scene, char **parts, t_vec3 *v0, t_vec3 *v1, t_vec3 *v2)
+void	parse_triangle_data(t_scene *scene, char **parts, \
+								t_triangle *triangle, t_color *color)
 {
-	if (!read_vector(parts[1], v0))
+	if (!read_vector(parts[1], &triangle->v0))
 	{
 		free_split(parts);
-		parse_error(scene, "Invalid format for triangle vertex 1. Expected: x,y,z");
+		parse_error(scene, "Expected for triangle vertex 1: x,y,z");
 	}
-	if (!read_vector(parts[2], v1))
+	if (!read_vector(parts[2], &triangle->v1))
 	{
 		free_split(parts);
-		parse_error(scene, "Invalid format for triangle vertex 2. Expected: x,y,z");
+		parse_error(scene, "Expected for triangle vertex 2: x,y,z");
 	}
-	if (!read_vector(parts[3], v2))
+	if (!read_vector(parts[3], &triangle->v2))
 	{
 		free_split(parts);
-		parse_error(scene, "Invalid format for triangle vertex 3. Expected: x,y,z");
+		parse_error(scene, "Expected for triangle vertex 3: x,y,z");
+	}
+	if (!read_color(parts[4], color))
+	{
+		free_split(parts);
+		parse_error(scene, "Expected for triangle color: r,g,b");
 	}
 }
 
 int	parse_triangle(t_scene *scene, char *line)
 {
-	char	**parts;
+	char		**parts;
 	t_object	*triangle;
-	t_vec3		v0;
-	t_vec3		v1;
-	t_vec3		v2;
+	t_triangle	triangle_data;
 	t_color		color;
 	char		*material_block;
 
@@ -48,17 +52,13 @@ int	parse_triangle(t_scene *scene, char *line)
 	if (!parts)
 		parse_error(scene, "Failed to split line");
 	check_parts_count(scene, parts, 5, "triangle");
-	parse_triangle_data(scene, parts, &v0, &v1, &v2);
-	if (!read_color(parts[4], &color))
-	{
-		free_split(parts);
-		parse_error(scene, "Invalid format for triangle color. Expected: r,g,b");
-	}
+	parse_triangle_data(scene, parts, &triangle_data, &color);
 	free_split(parts);
-	triangle = create_triangle(v0, v1, v2, color);
+	triangle = create_triangle(triangle_data.v0, triangle_data.v1, \
+								triangle_data.v2, color);
 	if (!triangle)
 		parse_error(scene, "Failed to create triangle");
-	if(material_block)
+	if (material_block)
 		parse_material_properties(scene, material_block, &triangle->material);
 	add_object(scene, triangle);
 	return (1);
