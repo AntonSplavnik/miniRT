dddc6cb47798b8ebbedfbae2ae304d5a452a03bd
Audrey Billoteau <abillote@c4b5c6.42berlin.de>
2025-06-11 14:23:50 +0200

    Parser textures refactored and nomrinette ok

--- menger_sponge/cleaned_version/srcs/parser/parser_texture.c
+++ menger_sponge/cleaned_version/srcs/parser/parser_texture.c
@@ -6,72 +6,72 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:36:27 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/04 16:38:26 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/11 14:23:22 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-t_texture *create_texture(t_scene *scene, const char *filename)
+void	fill_texture_mlx(t_texture *texture, mlx_texture_t *mlx_texture, \
+							t_scene *scene)
 {
-	t_texture *texture;
-	mlx_texture_t *mlx_texture;
+	texture->width = mlx_texture->width;
+	texture->height = mlx_texture->height;
+	texture->mlx_texture = mlx_texture;
+	texture->data = malloc(mlx_texture->width * mlx_texture->height * 4);
+	if (!texture->data)
+	{
+		free_texture_mlx(texture);
+		parse_error(scene, "Failed to allocate memory for texture data");
+	}
+	memcpy(texture->data, mlx_texture->pixels, \
+				mlx_texture->width * mlx_texture->height * 4);
+}
+
+t_texture	*create_texture(t_scene *scene, const char *filename)
+{
+	t_texture		*texture;
+	mlx_texture_t	*mlx_texture;
 
 	texture = malloc(sizeof(t_texture));
 	if (!texture)
-		return NULL;
+		parse_error(scene, "Failed to allocate memory for texture");
+	*texture = (t_texture){.filename = NULL, .mlx_texture = NULL, \
+								.data = NULL, .width = 0, .height = 0};
 	texture->filename = ft_strdup(filename);
 	if (!texture->filename)
 	{
 		free(texture);
-		return NULL;
+		parse_error(scene, "Failed to allocate memory for texture");
 	}
 	mlx_texture = mlx_load_png(texture->filename);
 	if (!mlx_texture)
 	{
-		free(texture->filename);
-		free(texture);
+		free_texture_mlx(texture);
 		parse_error(scene, "Failed to load texture from file");
 	}
-	texture->width = mlx_texture->width;
-	texture->height = mlx_texture->height;
-	texture->mlx_texture = mlx_texture;
-	texture->data = malloc(mlx_texture->width * mlx_texture->height * 4); // Assuming RGBA format
-	if (!texture->data)
-	{
-		printf("Error: Could not allocate memory for texture data\n");
-		free_texture_mlx(texture);
-		return NULL;
-	}
-	memcpy(texture->data, mlx_texture->pixels, mlx_texture->width * mlx_texture->height * 4);
-	return texture;
+	fill_texture_mlx(texture, mlx_texture, scene);
+	return (texture);
 }
 
-void free_texture_mlx(t_texture *texture)
+void	free_texture_mlx(t_texture *texture)
 {
 	if (!texture)
-		return;
-
-	// Free the filename if it exists
+		return ;
 	if (texture->filename)
 	{
 		free(texture->filename);
 		texture->filename = NULL;
 	}
-
-	// Free the MLX texture if it exists
 	if (texture->mlx_texture)
 	{
 		mlx_delete_texture(texture->mlx_texture);
 		texture->mlx_texture = NULL;
 	}
-
-	// Free the data if it exists
 	if (texture->data)
 	{
 		free(texture->data);
 		texture->data = NULL;
 	}
-
 	free(texture);
 }
