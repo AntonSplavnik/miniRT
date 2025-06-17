5820540e0a343b0c8f778354b5250bf522a3ab03
LazyPixel <antonsplavnik@gmail.com>
2025-06-17 10:57:04 +0200

    Refactor: Clean up key callback functionality in camera movement
    - Removed unnecessary debug print statements for camera position in the key callback function.
    - Simplified the key handling logic by eliminating the empty case for the 'C' key.

--- miniRT/srcs/controls/keys.c
+++ miniRT/srcs/controls/keys.c
@@ -14,20 +14,14 @@ void	key_callback(mlx_key_data_t keydata, void* param)
     {
         mlx_close_window(scene->app.mlx);
     }
-    else if (keydata.key == MLX_KEY_C)
-    {
-    }
     else if (keydata.key == MLX_KEY_W)
     {
-        printf("camera position before: \n x - %f\n y - %f\n z - %f\n", scene->camera.position.x, scene->camera.position.y, scene->camera.position.z);
         scene->camera.position = vec3_add(scene->camera.position, 
             vec3_scale(get_forward_vector(scene->camera.rotation), scene->camera.movement_speed));
         render_scene(scene);
-        printf("camera position before: \n x - %f\n y - %f\n z - %f\n", scene->camera.position.x, scene->camera.position.y, scene->camera.position.z);
     }
     else if (keydata.key == MLX_KEY_S)
     {
-        printf("S key is pressed\n");
         scene->camera.position = vec3_subtract(scene->camera.position, 
             vec3_scale(get_forward_vector(scene->camera.rotation), scene->camera.movement_speed));
         render_scene(scene);
