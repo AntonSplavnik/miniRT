98bd91d0efdee0b4b9d671032909adec16b6d553
Audrey Billoteau <abillote@c4a8c1.42berlin.de>
2025-06-20 14:46:12 +0200

    Cone normal ok

--- miniRT/srcs/objects/cone/cone_normal.c
+++ miniRT/srcs/objects/cone/cone_normal.c
@@ -6,35 +6,63 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 10:50:45 by abillote          #+#    #+#             */
-/*   Updated: 2025/06/20 11:18:31 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/20 14:45:14 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/miniRT.h"
 
-
 /**
- * Check if point is on the cone base
+ * Determines if a point lies on the cone's base or very close to the base edge
+ *
+ * This function performs two checks:
+ * 1. If the point is on the base circular disc:
+ *    - Measures distance from point to base plane
+ *    - Measures radial distance from point to base center
+ *    - If both are within thresholds, point is on the base
+ * 2. If the point is at the top edge of the cone's body:
+ *    - Calculates projection along cone's axis
+ *    - If projection is very close to height, point is at the rim
+ *
+ * @param point       The point to check
+ * @param cone        The cone object
+ * @param base_center The pre-calculated center of the cone's base
+ *
+ * @return 1 if point is on base or rim, 0 otherwise
  */
 static int	is_on_cone_base(t_vec3 point, t_cone cone, t_vec3 base_center)
 {
 	t_vec3	point_to_base;
 	double	dist_to_base_plane;
 	double	dist_to_base_center;
+	double	projection;
+	t_vec3	to_point;
 
 	point_to_base = vec3_subtract(point, base_center);
 	dist_to_base_plane = fabs(vec3_dot(point_to_base, cone.axis));
 	dist_to_base_center = vec3_length(point_to_base);
-	if (dist_to_base_plane < 0.01 && dist_to_base_center <= cone.radius * 1.01)
+	if (dist_to_base_plane < 0.00001 && dist_to_base_center \
+				<= cone.radius * 1.01)
 		return (1);
-	point_to_base = vec3_subtract(point, cone.apex);
-	if (calc_projection(point, cone, &point_to_base) >= cone.height - 0.005)
+	to_point = vec3_subtract(point, cone.apex);
+	projection = vec3_dot(to_point, cone.axis);
+	if (projection >= cone.height - 0.005)
 		return (1);
 	return (0);
 }
 
 /**
- * Clamp projection value to valid range
+ * Constrains a projection value to avoid numerical precision issues
+ *
+ * This function ensures that the projection value stays within safe
+ * boundaries to avoid artifacts at the apex and base of the cone.
+ * - Values too close to 0 are clamped to a minimum threshold
+ * - Values too close to height are clamped to just below height
+ *
+ * @param projection The raw projection value along cone axis
+ * @param height     The height of the cone
+ *
+ * @return The clamped projection value
  */
 static double	clamp_projection(double projection, double height)
 {
@@ -49,21 +77,19 @@ static double	clamp_projection(double projection, double height)
 }
 
 /**
- * Create perpendicular vector for degenerate cases
- */
-static t_vec3	create_perpendicular(t_vec3 axis)
-{
-	t_vec3	perp;
-
-	if (fabs(axis.x) < 0.9)
-		perp = vec3_cross(axis, vec3_create(1, 0, 0));
-	else
-		perp = vec3_cross(axis, vec3_create(0, 1, 0));
-	return (vec3_normalize(perp));
-}
-
-/**
- * Calculate radial component for surface normal
+ * Calculates the radial component for cone surface normal
+ *
+ * This function:
+ * 1. Computes the vector from axis point to surface point
+ * 2. Handles the degenerate case when point is too close to axis
+ *    - Creates an arbitrary perpendicular vector using cross product
+ * 3. Otherwise normalizes the radial vector
+ *
+ * @param point      The point on cone surface
+ * @param axis_point The corresponding point on cone axis
+ * @param cone       The cone object
+ *
+ * @return The normalized radial vector
  */
 static t_vec3	calc_radial(t_vec3 point, t_vec3 axis_point, t_cone cone)
 {
@@ -73,14 +99,35 @@ static t_vec3	calc_radial(t_vec3 point, t_vec3 axis_point, t_cone cone)
 	radial = vec3_subtract(point, axis_point);
 	radial_length = vec3_length(radial);
 	if (radial_length < 0.0001)
-		return (create_perpendicular(cone.axis));
+	{
+		if (fabs(cone.axis.x) < 0.9)
+			return (vec3_normalize(vec3_cross(cone.axis, \
+						vec3_create(1, 0, 0))));
+		else
+			return (vec3_normalize(vec3_cross(cone.axis, \
+						vec3_create(0, 1, 0))));
+	}
 	return (vec3_scale(radial, 1.0 / radial_length));
 }
 
 /**
- * Calculate surface normal for cone body
+ * Computes the surface normal for a point on the cone body
+ *
+ * The normal calculation follows these steps:
+ * 1. Clamp projection to avoid numerical issues
+ * 2. Find the point on cone axis at the same height
+ * 3. Calculate radial component (vector perpendicular to axis)
+ * 4. Calculate cone slope (ratio of radius to height)
+ * 5. Compute normal by subtracting scaled axis from radial vector
+ *
+ * @param point      The point on cone surface
+ * @param cone       The cone object
+ * @param projection The projection of point along cone axis
+ *
+ * @return The normalized surface normal vector
  */
-static t_vec3	calc_surface_normal(t_vec3 point, t_cone cone, double projection)
+static t_vec3	calc_surface_normal(t_vec3 point, \
+					t_cone cone, double projection)
 {
 	t_vec3	axis_point;
 	t_vec3	radial;
@@ -96,7 +143,20 @@ static t_vec3	calc_surface_normal(t_vec3 point, t_cone cone, double projection)
 }
 
 /**
- * Calculate normal vector at a point on a cone
+ * Main function to calculate normal vector at any point on a cone
+ *
+ * This function determines the appropriate normal vector based on
+ * where the point lies on the cone:
+ * - If point is on base or at rim: returns the cone axis
+ * - Otherwise calculates surface normal for cone body
+ *
+ * The function handles all edge cases and ensures returned normals
+ * are always properly normalized.
+ *
+ * @param point The intersection point on cone surface
+ * @param cone  The cone object
+ *
+ * @return The normal vector at the given point, always normalized
  */
 t_vec3	cone_normal_at_point(t_vec3 point, t_cone cone)
 {
--- miniRT/srcs/objects/mesh/mesh_intersect.c
+++ miniRT/srcs/objects/mesh/mesh_intersect.c
@@ -6,55 +6,11 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 16:20:00 by abillote          #+#    #+#             */
-/*   Updated: 2025/05/16 11:16:22 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/20 14:11:28 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/miniRT.h"
-
-///**
-// * Finds the closest intersection between a ray and a mesh
-// *
-// * @param ray The ray to test
-// * @param mesh The mesh to test
-// * @param t Pointer to store the distance to intersection
-// * @param triangle_idx Pointer to store the index of the hit triangle
-// * @return 1 if there's an intersection, 0 otherwise
-// */
-//int	ray_mesh_intersect(t_ray ray, t_mesh mesh, double *t, int *triangle_idx)
-//{
-//	int		i;
-//	double	closest_t;
-//	double	current_t;
-//	int		hit_something;
-
-//	closest_t = INFINITY;
-//	hit_something = 0;
-//	*triangle_idx = -1;
-
-//	// Check intersection with each triangle in the mesh
-//	for (i = 0; i < mesh.triangle_count; i++)
-//	{
-//		// Test ray against this triangle
-//		if (ray_triangle_intersect(ray, mesh.triangles[i], &current_t))
-//		{
-//			// Found a closer intersection?
-//			if (current_t < closest_t)
-//			{
-//				closest_t = current_t;
-//				*triangle_idx = i;
-//				hit_something = 1;
-//			}
-//		}
-//	}
-
-//	if (hit_something)
-//	{
-//		*t = closest_t;
-//		return (1);
-//	}
-//	return (0);
-//}
 
 //New version to rotate properly each triangle in the mesh according to the mesh's rotation
 int ray_mesh_intersect(t_ray ray, t_mesh mesh, double *t, int *triangle_idx)
