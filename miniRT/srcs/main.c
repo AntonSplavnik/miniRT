70cd5d96aba22a92b7a24728e4eeee7042e1b7c5
LazyPixel <antonsplavnik@gmail.com>
2025-06-24 19:18:17 +0200

    refactor: clean up main.c and remove unused BVH source file

--- miniRT/srcs/main.c
+++ miniRT/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: antonsplavnik <antonsplavnik@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:16:31 by asplavni          #+#    #+#             */
-/*   Updated: 2025/06/09 14:19:22 by abillote         ###   ########.fr       */
+/*   Updated: 2025/06/24 19:17:16 by antonsplavn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,22 @@ void init_scene_bvh(t_scene *scene);
 void	start_raytracer(t_scene *scene, char *filename)
 {
 	scene->name = filename;
-	
+
 	// Initialize miniRT
 	init_data(scene);
 	init_mlx(scene);
-	
+
 	// Parse scene file
-	if (filename)
+
+	if (parse_scene_file(filename, scene) != 0)
 	{
-		if (parse_scene_file(filename, scene) != 0)
-		{
-			write_string_to_file_descriptor("Error\nParsing scene file failed.\n", 2);
-			exit(1);
-		}
+		write_string_to_file_descriptor("Error\nParsing scene file failed.\n", 2);
+		exit(1);
 	}
-	else
-	{
-		// For testing without a scene file, load a default scene
-		set_up_scene_plane(scene);
-	}
-	
+
 	// Initialize BVH after all objects are loaded
 	init_scene_bvh(scene);
-	
+
 	// Set up UI and hooks
 	init_ui(scene);
 	setup_hooks(scene);
@@ -50,10 +43,10 @@ void	start_raytracer(t_scene *scene, char *filename)
 	// mlx_loop_hook(scene->app.mlx, &render_scene, scene);
 
 	render_scene(scene);
-	
+
 	// Start main loop
 	mlx_loop(scene->app.mlx);
-	
+
 	// Clean up
 	cleanup_scene(scene);
 }
